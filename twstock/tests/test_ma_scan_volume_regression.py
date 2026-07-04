05f14a70f00e3a7e4cae272b4717252d2ef65212
yungtang <yungtang2011@gmail.com>
2026-07-04 21:15:00 +0800

    tests: add regression for scan min_volume; ensure breakout sample

--- twstock/tests/test_ma_scan_volume_regression.py
+++ twstock/tests/test_ma_scan_volume_regression.py
@@ -56,8 +56,9 @@ def test_scan_excludes_below_min_volume():
 
     # stock A: low latest volume
     a = '9001'
-    closes = [10 + i * 0.1 for i in range(205)]
-    vols_a = [600000] * 204 + [4000]  # last day low
+    # make price series so that the latest day is a clear breakout above MA
+    closes = [1.0] * 203 + [1.0, 200.0]
+    vols_a = [600000] * 204 + [4000]  # last day low (4 張)
     for d, c, v in zip(dates, closes, vols_a):
         conn.execute(
             'INSERT INTO stock_history (stock_id, date, open, high, low, close, volume, amount) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
@@ -67,7 +68,7 @@ def test_scan_excludes_below_min_volume():
 
     # stock B: valid high volume
     b = '9002'
-    vols_b = [600000] * 205
+    vols_b = [600000] * 204 + [700000]  # ensure curr_vol > prev_vol
     for d, c, v in zip(dates, closes, vols_b):
         conn.execute(
             'INSERT INTO stock_history (stock_id, date, open, high, low, close, volume, amount) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
